6075f51992ec3765a1fac0c94238d45cea85a602
sjdj0147 <sjdj014@gmail.com>
2024-03-27 15:16:18 +0900

    feat: 시작

--- controller/imweb.py
+++ controller/imweb.py
@@ -27,7 +27,7 @@ class ImwebScraper:
             "User-Agent": "Mozilla/5.0(Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
         }
         driver = webdriver.Chrome(
-            "/Users/onthelook/dev_assigment/chromedriver", options=self.options
+            "/Users/onthelook/Desktop/chromedriver", options=self.options
         )
         driver.get(product_url)
         time.sleep(1)
